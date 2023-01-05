b07aab4f448cfca6ba52ebc687be2b872ede4389
kezia2909 <c14190048@john.petra.ac.id>
2023-01-05 21:06:52 +0700

    konek python berhasil | android done | build android done

--- flutter_application_recommendation/lib/pages/home_page.dart
+++ flutter_application_recommendation/lib/pages/home_page.dart
@@ -1,3 +1,10 @@
+import 'dart:convert';
+import 'dart:io';
+import 'dart:ui' as ui;
+
+import 'package:flutter_application_recommendation/services/painter_service.dart';
+import 'package:http/http.dart' as http;
+import 'package:image_picker/image_picker.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_recommendation/pages/login_page.dart';
@@ -15,6 +22,60 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   late User user = widget.firebaseUser;
+
+// Selected Image storing in a Variable
+  File? _selectedImage;
+  // Faces Coordinates List wrt x, y, w, h
+  List<List<int>> facesCoordinates = <List<int>>[];
+  // Boolean value whether the face is detected or not
+  bool get isFaceDetected => facesCoordinates.isEmpty ? false : true;
+
+  late String uniqueCode;
+  final uniqueCodeController = TextEditingController();
+  late String pathNgrok;
+
+  Future<http.Response> getFaceCoordinate(File file, String link) async {
+    ///MultiPart request
+    String filename = file.path.split('/').last;
+    var request = http.MultipartRequest(
+      'POST',
+      Uri.parse(link),
+    );
+    Map<String, String> headers = {"Content-type": "multipart/form-data"};
+    request.files.add(
+      http.MultipartFile(
+        'image',
+        file.readAsBytes().asStream(),
+        file.lengthSync(),
+        filename: filename,
+      ),
+    );
+    request.headers.addAll(headers);
+    print("request: " + request.toString());
+    var res = await request.send();
+    var response = await http.Response.fromStream(res);
+    print("This is response:" + response.body);
+    print("This is response: ${res.statusCode} ");
+    print("This is response: ${res.statusCode} ");
+    return response;
+  }
+
+  Future<void> _addImage() async {
+    facesCoordinates.clear();
+    final image =
+        await ImagePicker.platform.pickImage(source: ImageSource.gallery);
+    if (image != null) {
+      _selectedImage = File(image.path);
+    }
+    setState(() {});
+  }
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    uniqueCodeController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -59,10 +120,98 @@ class _HomePageState extends State<HomePage> {
                       await AuthServices.logOut();
                     }),
                 const Text("aaa"),
+                Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: TextField(
+                        controller: uniqueCodeController,
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        uniqueCode = uniqueCodeController.text;
+                        pathNgrok = "https://" +
+                            uniqueCode +
+                            ".ap.ngrok.io/face_detection";
+                        showDialog(
+                          context: context,
+                          builder: (context) {
+                            return AlertDialog(
+                              // Retrieve the text the that user has entered by using the
+                              // TextEditingController.
+                              content: Text(uniqueCode),
+                            );
+                          },
+                        );
+                      },
+                      child: Text("submit"),
+                    ),
+                  ],
+                ),
+                if (_selectedImage == null)
+                  const Text(
+                    'Please Select a Image',
+                  )
+                else
+                  Column(
+                    children: [
+                      SizedBox(
+                          width: MediaQuery.of(context).size.width * 0.8,
+                          child: Image.file(File(_selectedImage!.path))),
+                      ElevatedButton(
+                        onPressed: () async {
+                          final res = await getFaceCoordinate(
+                              File(_selectedImage!.path), pathNgrok);
+                          debugPrint(res.body);
+                          final val = jsonDecode(res.body);
+                          List<List<int>> data = [];
+                          for (var items in val['faces']) {
+                            List<int> s = [];
+                            for (var item in items as List) {
+                              s.add(int.parse("$item"));
+                            }
+                            data.add(s);
+                          }
+                          debugPrint("$data");
+                          facesCoordinates = data;
+
+                          setState(() {});
+                        },
+                        child: const Text("aaaa"),
+                      ),
+                      isFaceDetected
+                          ? FutureBuilder<ui.Image>(
+                              future: decodeImageFromList(
+                                  File(_selectedImage!.path).readAsBytesSync()),
+                              builder: (context, snapshot) {
+                                if (!snapshot.hasData) {
+                                  return Container();
+                                }
+                                return SizedBox(
+                                  width: snapshot.data!.width.toDouble(),
+                                  height: snapshot.data!.height.toDouble(),
+                                  child: CustomPaint(
+                                    painter: FacePainter(
+                                      snapshot.data!,
+                                      facesCoordinates,
+                                    ),
+                                  ),
+                                );
+                              })
+                          : const Text(
+                              "No Face Detected or click Detect Face Button"),
+                    ],
+                  ),
               ],
             ),
           ),
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _addImage,
+        tooltip: 'Image',
+        child: const Icon(Icons.add),
       ),
     );
   }
