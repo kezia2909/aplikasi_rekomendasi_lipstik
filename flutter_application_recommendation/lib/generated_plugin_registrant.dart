a4b1e20de952615d4c3410a9705f7a80a7f19201
kezia2909 <c14190048@john.petra.ac.id>
2023-05-07 17:39:25 +0700

    splash screen oke

--- flutter_application_recommendation/lib/generated_plugin_registrant.dart
+++ flutter_application_recommendation/lib/generated_plugin_registrant.dart
@@ -10,6 +10,7 @@ import 'package:file_picker/_internal/file_picker_web.dart';
 import 'package:firebase_auth_web/firebase_auth_web.dart';
 import 'package:firebase_core_web/firebase_core_web.dart';
 import 'package:firebase_storage_web/firebase_storage_web.dart';
+import 'package:flutter_native_splash/flutter_native_splash_web.dart';
 import 'package:image_picker_for_web/image_picker_for_web.dart';
 
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
@@ -21,6 +22,7 @@ void registerPlugins(Registrar registrar) {
   FirebaseAuthWeb.registerWith(registrar);
   FirebaseCoreWeb.registerWith(registrar);
   FirebaseStorageWeb.registerWith(registrar);
+  FlutterNativeSplashWeb.registerWith(registrar);
   ImagePickerPlugin.registerWith(registrar);
   registrar.registerMessageHandler();
 }
